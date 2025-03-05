d38616fede5d751b62ae6c386cc46d1cd4467be9
Gildas <herbreteau.gildas@hotmail.fr>
2025-03-05 16:59:17 +0100

    Deuxième push

--- src/app/generations/generations.component.ts
+++ src/app/generations/generations.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './generations.component.css'
 })
 export class GenerationsComponent {
+  public titre: string = 'LES DIFFÉRENTES GÉNÉRATIONS :';
+
+  public generations: string[] = [
+    'Génération I',
+    'Génération II',
+    'Génération III',
+    'Génération IV',
+    'Génération V',
+    'Génération VI',
+    'Génération VII'
+  ];
 
 }
--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -8,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class PokemonsComponent {
 
+  public pokemons = [{
+    'numero': '001',
+    'nom': 'Bulbizarre',
+    'categorie': 'graine',
+    'type': 'plante.poison',
+    'taille': '0,7',
+    'poids': '6,9'
+  },
+  {
+    'numero': '002',
+    'nom': 'Herbizarre',
+    'categorie': 'graine',
+    'type': 'plante.poison',
+    'taille': '1',
+    'poids': '13'
+  },
+  {
+    'numero': '003',
+    'nom': 'Florizarre',
+    'categorie': 'graine',
+    'type': 'plante.poison',
+    'taille': '2',
+    'poids': '100'
+  },
+  {
+    'numero': '004',
+    'nom': 'Salamèche',
+    'categorie': 'lézard',
+    'type': 'feu',
+    'taille': '0,6',
+    'poids': '8,5'
+  },
+  {
+    'numero': '005',
+    'nom': 'Reptincel',
+    'categorie': 'flamme',
+    'type': 'feu',
+    'taille': '1,1',
+    'poids': '19'
+  },
+  {
+    'numero': '006',
+    'nom': 'Dracaufeu',
+    'categorie': 'flamme',
+    'type': 'feu.vol',
+    'taille': '1,7',
+    'poids': '90,5'
+  }]
 }
